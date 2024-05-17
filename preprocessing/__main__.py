42d5ef79d6914dfc614887500d2744a50682b713
shinofumijp <electric.spanish.shinofumi.335@gmail.com>
2024-05-17 15:31:24 +0900

    Add flag

--- preprocessing/__main__.py
+++ preprocessing/__main__.py
@@ -7,15 +7,16 @@ from preprocessing.dedup.dedup import exec_deduplication
 from preprocessing.filters.pipeline import execute_filtering, execute_url_dedup
 
 
-def execute_preprocessing(input_dir: str, output_base: str, filtering: bool, dedup: bool, *, logger=None):
+def execute_preprocessing(input_dir: str, output_base: str, url_dedup: bool, filtering: bool, dedup: bool, *, logger=None):
     logger = logger or Logger.get_logger(__name__, logdir=os.path.join(output_base, "log"))
-    if filtering:
+    if url_dedup:
         logger.info("Executing url dedup")
         start = datetime.now()
         input_dir = execute_url_dedup(input_dir=input_dir, output_base=output_base, logger=logger)
         end = datetime.now()
         logger.info(f"Finished url dedup in {end - start}")
 
+    if filtering:
         logger.info("Executing filtering")
         start = datetime.now()
         input_dir = execute_filtering(input_dir=input_dir, output_base=output_base, logger=logger)
@@ -36,6 +37,8 @@ def arg_parser():
                         help='The input directory containing documents to process', required=True)
     parser.add_argument('--output_dir', type=str,
                         help='The input file containing documents to process', required=False, default="./output")
+    parser.add_argument('--url_dedup', type=bool, help='Whether to execute url deduplication',
+                        required=False, default=True)
     parser.add_argument('--filtering', type=bool, help='Whether to execute filtering', required=False, default=True)
     parser.add_argument('--dedup', type=bool, help='Whether to execute deduplication', required=False, default=True)
     parser.add_argument('--verbose', type=bool, help='Verbose mode', required=False, default=False)
@@ -52,7 +55,8 @@ def main():
 
     logger = Logger.get_logger(name=__name__, logdir=logdir, verbose=args.verbose)
 
-    execute_preprocessing(args.input_dir, output_base, filtering=args.filtering, dedup=args.dedup, logger=logger)
+    execute_preprocessing(args.input_dir, output_base, url_dedup=args.url_dedup,
+                          filtering=args.filtering, dedup=args.dedup, logger=logger)
 
 
 if __name__ == "__main__":
